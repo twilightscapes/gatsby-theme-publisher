f3636f921d6ed1c7f9b55dc26a70a8b5b3705a24
Todd <ihatespam@gmail.com>
2019-08-21 10:29:57 -0500

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -18,7 +18,7 @@ const MENU_QUERY = graphql`
 
   query GET_MENU_ITEMS {
     wpgraphql {
-      menuItems(where: { location: menu-1 }) {
+      menuItems(where: { location: PRIMARY }) {
         nodes {
           ...MenuFields
           childItems {
