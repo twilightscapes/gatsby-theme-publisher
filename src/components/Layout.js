4766fac1825cac84d79c6c448a0f565bd8a2f3c2
Todd <ihatespam@gmail.com>
2019-08-22 21:42:38 -0500

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import PropTypes from 'prop-types';
-import "../css/tailwind-customizations.css"
+// import "../css/tailwind-customizations.css"
 import "../css/index.scss"
 
 const Layout = ({ children }) => (
