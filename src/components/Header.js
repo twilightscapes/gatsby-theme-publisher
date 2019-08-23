78dc06d2f76b9814674bdec78f8ecca52e777533
Todd <ihatespam@gmail.com>
2019-08-23 02:38:59 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,25 +10,18 @@ const Header = () => {
     <section id="section-header">
     <header
       id="masthead"
-      className="mb-8 w-full clearfix bg-gray-900"
+      className="site-header mb-8 w-full clearfix bg-gray-900"
       role="banner"
     >
-      <div className="flex inset-x-0 h-16 items-center max-w-6xl m-auto">
-        <div className="w-full max-w-screen-xl relative mx-auto px-6">
-          <div className="flex items-center justify-between text-white">
-            <div className="lg:w-1/4 xl:w-1/5 mr-4">
+     
               <Logo data={title} />
-            </div>
+
 
             {/* <div>
                       <input className="rounded-lg bg-gray-200 p-4 h-8" type="text" />
                     </div> */}
             <div>
               <Menu />
-            </div>
-          </div>
-        </div>
-      </div>
     </header>
     </section>
   )
