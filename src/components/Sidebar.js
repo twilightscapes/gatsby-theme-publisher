69a00e4e312fae92b49cf714e2cfdaa2cb8555bf
twilightscapes <ihatespam@gmail.com>
2019-09-08 03:57:20 -0500

    side images

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,26 +1,23 @@
 import React from 'react';
+import twButton from '../images/twilightscapes-button.jpg';
+import yonButton from '../images/yearofnight-button.jpg';
 
 
 
-const Sidebar = (props) => (
-	<img src={props.twButton} alt="" />
-	)
 
 export default () => (
 	    
     <aside id="section-aside">
     <h3>Todd Network</h3>
-    <p>Visit other sites by Todd Lambert</p>
-    <br />
-    <br />
-     <Sidebar
-    twButton="/static/twilightscapes-button.jpg"
-    />
-    <br />
-    <Sidebar
-    twButton="/static/yearofnight-button.jpg"
-    />
+    <p>Visit other sites by Todd Lambert:</p>
+
   
+
+    <a href="https://twilightscapes.com"><img src={twButton} alt="Twilightscapes" /></a>
+
+  <br />
+  <br />
+  <a href="https://twilightscapes.com/yearofnight/"><img src={yonButton} alt="Year of Night" /></a>
 
     
     </aside>
