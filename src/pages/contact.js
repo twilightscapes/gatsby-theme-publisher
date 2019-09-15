cf9315a6d77f63583a9b978e08136cfe542933eb
twilightscapes <ihatespam@gmail.com>
2019-09-15 09:25:41 -0500

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react"
+import Layout from "../components/Layout"
+
 
 const Contact = (props) => (
-    <section id="contact">
+	<Layout>
+
         <div className="inner">
             <section>
-                <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
+                <form name="contact" method="post" action="../" data-netlify="true" data-netlify-honeypot="bot-field">
 
                     <input type="hidden" name="bot-field" />
 
@@ -26,33 +29,9 @@ const Contact = (props) => (
                     </ul>
                 </form>
             </section>
-            <section className="split">
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-envelope"></span>
-                        <h3>Email</h3>
-                        <a href="#">information@untitled.tld</a>
-                    </div>
-                </section>
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-phone"></span>
-                        <h3>Phone</h3>
-                        <span>(000) 000-0000 x12387</span>
-                    </div>
-                </section>
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-home"></span>
-                        <h3>Address</h3>
-                        <span>1234 Somewhere Road #5432<br />
-                        Nashville, TN 00000<br />
-                        United States of America</span>
-                    </div>
-                </section>
-            </section>
+
         </div>
-    </section>
+    </Layout>
 )
 
 export default Contact
